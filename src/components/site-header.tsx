b178cf7e422830261f922dcd8a6d3a562415707f
SammyFerreiraa <sammyfe2021@gmail.com>
2025-05-29 20:54:45 -0300

    :sparkles: feat: add exit button on header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -2,11 +2,13 @@
 
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
+import { Button } from "./ui/button"
 
 export function SiteHeader() {
   const location = usePathname()
   const title = location.split("/").pop()
+  const router = useRouter()
 
   return (
     <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear rounded-t-lg">
@@ -17,6 +19,7 @@ export function SiteHeader() {
           className="mx-2 data-[orientation=vertical]:h-4"
         />
         <h1 className="text-base font-medium capitalize">{title}</h1>
+        <Button className="ml-auto" variant={"ghost"} onClick={() => router.push("/")}>Sair</Button>
       </div>
     </header>
   )
