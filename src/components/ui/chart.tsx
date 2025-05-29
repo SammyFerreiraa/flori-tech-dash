9e903f5e2812c941b5f15f6061da19314db04d45
SammyFerreiraa <sammyfe2021@gmail.com>
2025-05-29 20:39:56 -0300

    :recycle: refact: chance style colors

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -228,7 +228,7 @@ function ChartTooltipContent({
                   >
                     <div className="grid gap-1.5">
                       {nestLabel ? tooltipLabel : null}
-                      <span className="text-black">
+                      <span className="text-foreground">
                         {itemConfig?.label || item.name}
                       </span>
                     </div>
